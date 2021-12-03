cb0d4c6a137dd87df62c4ae645d2557ee1b2265b
nyvs <jwstrathmann@gmail.com>
2021-12-03 03:40:59 +0100

    magical pain-relieving overhaul

--- src/lib.rs
+++ src/lib.rs
@@ -1,170 +1,208 @@
 #![doc(html_root_url = "https://docs.rs/inv-sys/0.2.2")]
 
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Inv<T> {
-	slots: Vec<Option<(T, usize)>>,
-	maxslots: usize,
-	selected_slot: usize
-}
+use std::fmt::Debug;
 
 pub trait Stacksize {
 	fn get_max_stacksize(&self) -> usize;
 }
 
-impl<T> Inv<T> where 
-T: Stacksize + Eq + Clone {
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Inv<T> {
+	slots: Vec<Slot<T>>,
+	maxslots: usize,
+	selected_slot: usize
+}
+
+impl<T> IntoIterator for Inv<T> {
+	type Item = Slot<T>;
+	type IntoIter = std::vec::IntoIter<Self::Item>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		self.slots.into_iter()
+	}
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Slot<T> {
+	inner: Option<ItemStack<T>>
+}
+
+impl<T> Slot<T>
+where T: Stacksize + Eq + Clone {
+	pub fn new_empty() -> Self {
+		Self {
+			inner: None
+		}
+	}
+
+	pub fn new(items: ItemStack<T>) -> Self {
+		Self {
+			inner: Some(items)
+		}
+	}
+
+	pub fn is_empty(&self) -> bool {
+		self.inner.is_none()
+	}
+
+	fn inner_mut(&mut self) -> &mut Option<ItemStack<T>> {
+		&mut self.inner
+	}
+
+	pub fn get_item_type(&self) -> Result<T, InvAccessErr> {
+		if let Some(inner) = &self.inner {
+			Ok(inner.get_type())
+		} else {
+			Err(InvAccessErr::SlotEmpty)
+		}
+	}
+
+	pub fn get_amount(&self) -> Result<usize, InvAccessErr> {
+		if let Some(inner) = &self.inner {
+			Ok(inner.get_amount())
+		} else {
+			Err(InvAccessErr::SlotEmpty)
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ItemStack<T> {
+	item_type: T,
+	amount: usize
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum StackErr<T>
+where T: Stacksize + Eq + Clone {
+	ItemTypeDoesNotMatch(ItemStack<T>),
+	StackSizeOverflow(ItemStack<T>)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum InvAccessErr {
+	SlotOutOfBounds,
+	SlotEmpty
+}
+
+pub type InvOverflow<T> = ItemStack<T>;
+
+impl<T> From<StackErr<T>> for ItemStack<T>
+where T: Stacksize + Eq + Clone {
+	fn from(item: StackErr<T>) -> Self {
+		match item {
+			StackErr::ItemTypeDoesNotMatch(x) => x,
+			StackErr::StackSizeOverflow(x) => x,
+		}
+	}
+}
+
+impl<T> ItemStack<T> 
+where T: Stacksize + Eq + Clone {
+	pub fn new(item_type: T, amount: usize) -> Self {
+		Self {
+			item_type,
+			amount
+		}
+	}
+
+	pub fn stacksize_split(&mut self) -> Result<(), ItemStack<T>> {
+		let max = self.item_type.get_max_stacksize();
+		if self.amount > max {
+			let rest = self.amount - max;
+			self.amount = max;
+			Err(
+				ItemStack::<T>::new(self.item_type.clone(), rest),
+			)
+		} else {
+			Ok(())
+		}
+	}
+
+	pub fn stack(&mut self, other: ItemStack<T>) -> Result<(), StackErr<T>> {
+		if other.item_type == self.item_type {
+			self.amount += other.amount;
+			self.stacksize_split().map_err(|err| StackErr::StackSizeOverflow(err))
+		} else {
+			Err(StackErr::ItemTypeDoesNotMatch(other))
+		}
+	}
+
+	pub fn get_type(&self) -> T {
+		self.item_type.clone()
+	}
+
+	pub fn get_amount(&self) -> usize {
+		self.amount
+	}
+}
+
+impl<T> Inv<T> 
+where T: Stacksize + Eq + Clone {
 	pub fn new(maxslots: usize) -> Self {
 		Inv {
-			slots: vec![None; maxslots],
+			slots: vec![Slot::new_empty(); maxslots],
 			maxslots: maxslots,
 			selected_slot: 0
 		}
 	}
 
-	/// Returns an Optional Item for the amount of an item, that could not be placed
-	pub fn place_at(&mut self, item: (T, usize), slot: usize) -> Option<(T, usize)> {
-		if self.slot_valid(slot) {
-			let item_at_spot = self.slots.get_mut(slot).unwrap().clone();
-			return match item_at_spot {
-				Some(ias) => {
-					let new_item = self.split_item_for_stacksize((item.0, item.1 + ias.1));
-					let ptr = self.slots.get_mut(slot).unwrap();
-					*ptr = new_item[0].clone();
-					new_item[1].clone()
-				},
-				None => {
-					let new_item = self.split_item_for_stacksize(item);
-					let ptr = self.slots.get_mut(slot).unwrap();
-					*ptr = new_item[0].clone();
-					new_item[1].clone()
+	fn auto_stack_inner_filled(&mut self, to_place: ItemStack<T>) -> Result<(), StackErr<T>> {
+		let mut state = to_place.clone();
+		for slot in self.slots.iter_mut() {
+			if let Some(used) = slot.inner_mut() {
+				match used.stack(state) {
+					Ok(()) => {
+						return Ok(());
+					},
+					Err(rest) => {
+						state = rest.into();
+					}
 				}
+			} else {
+				continue
 			}
 		}
-		Some(item)
+		Err(StackErr::StackSizeOverflow(state))
 	}
 
-	/// Splits an item, so that the first one is safe to store in the inventory, 
-	/// and the second is able to be processed by the user, if there is need
-	fn split_item_for_stacksize(&self, item: (T, usize)) -> [Option<(T, usize)>; 2] {
-		//check if item is over the stacksize
-		if item.1 == 0 {
-			return [None, None];
-		}
-
-		let max_stacksize = item.0.get_max_stacksize();
-		if item.1 > max_stacksize {
-			return [Some((item.0.clone(), max_stacksize)), Some((item.0, item.1 - max_stacksize))];
-		}
-
-		return [Some(item), None];
-	}
-
-	/// Tries to stack the item, returns remaining item.assert_eq!
-	/// If no item is found, that it can be stacked on, the place_at_first_free method is used
-	pub fn stack(&mut self, item: (T, usize)) -> Option<(T, usize)> {
-		for (i, slot) in self.slots.iter_mut().enumerate() {
-			match slot {
-				None => continue,
-				Some(slotval) => {
-					//if items are equal and slot not full (allow overflow)
-					if slotval.0 == item.0 
-					&& slotval.1 < slotval.0.get_max_stacksize() {
-						let rest = self.place_at(item, i);
-						if rest != None && self.can_be_filled_with(rest.clone().unwrap()) {
-							return self.stack(rest.unwrap());
-						}
-						else {
-							return rest;
-						}
+	fn auto_stack_inner_empty(&mut self, to_place: ItemStack<T>) -> Result<(), StackErr<T>> {
+		let mut state = to_place.clone();
+		for slot in self.slots.iter_mut() {
+			if slot.is_empty() {
+				match state.stacksize_split() {
+					Ok(()) => {
+						*slot.inner_mut() = Some(state);
+						return Ok(());
+					},
+					Err(rest) => {
+						*slot.inner_mut() = Some(state);
+						state = rest.into();
 					}
 				}
 			}
 		}
-		//no item found => try to use first free spot
-		self.place_at_first_free(item)
+		Err(StackErr::StackSizeOverflow(state))
 	}
 
-	/// If there still can be filled some slot with a minimum amount of 1, this should return true
-	pub fn can_be_filled_with(&self, item: (T, usize)) -> bool {
-		for slot in self.slots.iter() {
-			let x = match slot {
-				None => true,
-				Some(slotval) => slotval.0 == item.0 && slotval.1 < slotval.0.get_max_stacksize()
-			};
-			if x {
-				return true;
-			}
-		}
-		false
-	}
-
-	/// Try to place the item at the first free spot, returns the remaining item
-	pub fn place_at_first_free(&mut self, item: (T, usize)) -> Option<(T, usize)> {
-		for (i, slot) in self.slots.iter_mut().enumerate() {
-			match slot {
-				None => {
-					let rest = self.place_at(item, i);
-					if rest != None && self.can_be_filled_with(rest.clone().unwrap()) {
-						return self.place_at_first_free(rest.unwrap());
-					}
-					else {
-						return rest;
-					}
-				},
-				_ => continue
-			}
-		}
-		Some(item)
-	}
-
-	/// Get item of a specific slot position
-	pub fn get_slot(&self, slot: usize) -> Option<&(T, usize)> {
-		self.slots.get(slot).unwrap().as_ref()
-	}
-
-	/// Get item of the selected slot position
-	pub fn get_selected_slot(&self) -> Option<&(T, usize)> {
-		self.slots.get(self.selected_slot).unwrap().as_ref()
-	}
-
-	/// Set the selected slot
-	pub fn set_selected_slot(&mut self, slot: usize) -> bool {
-		return if self.slot_valid(slot) {
-			self.selected_slot = slot;
-			true
-		} else {
-			false
-		}
-	}
-
-	/// Decreases the amount of the item in the selected slot by 1
-	pub fn decrease_selected_slot(&mut self) -> bool {
-		let item = self.slots.get_mut(self.selected_slot).unwrap();
-		match item {
-			None => false,
-			Some(x) => {
-				x.1 -= 1;
-				if x.1 == 0 {
-					*item = None
+	pub fn auto_stack(&mut self, to_place: ItemStack<T>) -> Result<(), InvOverflow<T>> {
+		match self.auto_stack_inner_filled(to_place) {
+			Ok(()) => return Ok(()),
+			Err(rest) => {
+				match self.auto_stack_inner_empty(rest.into()) {
+					Ok(()) => return Ok(()),
+					Err(rest) => Err(rest.into()),
 				}
-				true
-			} 
+			},
 		}
 	}
 
-	//checks if a slot would be valid in this inventory
-	fn slot_valid(&self, slot: usize) -> bool {
-		slot < self.maxslots
+	pub fn get_slot(&self, index: usize) -> Result<&Slot<T>, InvAccessErr> {
+		match self.slots.get(index) {
+			Some(slot) => Ok(slot),
+			None => Err(InvAccessErr::SlotOutOfBounds)
+		}
 	}
-}
-
-impl<T> IntoIterator for Inv<T> {
-    type Item = Option<(T, usize)>;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.slots.into_iter()
-    }
 }
 
 #[cfg(test)]
--- src/test.rs
+++ src/test.rs
@@ -7,10 +7,71 @@ impl Stacksize for char {
 }
 
 #[test]
+fn auto_stack() {
+	let mut inv = Inv::<char>::new(4);
+	assert!(
+		inv.auto_stack(ItemStack::new('c', 6)).is_ok()
+	);
+
+	// 2*3 = 6
+	assert_eq!(
+		inv.get_slot(0).unwrap().get_amount(), 
+		Ok(3)
+	);
+	assert_eq!(
+		inv.get_slot(1).unwrap().get_amount(), 
+		Ok(3)
+	);
+}
+
+#[test]
+fn auto_stack_filled() {
+	let mut inv = Inv::<char>::new(4);
+
+	inv.auto_stack(
+		ItemStack::new('c', 1)
+	).ok();
+	inv.auto_stack(
+		ItemStack::new('a', 6)
+	).ok();
+	inv.auto_stack(
+		ItemStack::new('c', 5)
+	).ok();
+
+	// 3c + 3a + 3a + 3c
+	assert_eq!(
+		*inv.get_slot(0).unwrap(), 
+		Slot::new(ItemStack::new('c', 3))
+	);
+	assert_eq!(
+		*inv.get_slot(1).unwrap(), 
+		Slot::new(ItemStack::new('a', 3))
+	);
+	assert_eq!(
+		*inv.get_slot(2).unwrap(), 
+		Slot::new(ItemStack::new('a', 3))
+	);
+	assert_eq!(
+		*inv.get_slot(3).unwrap(), 
+		Slot::new(ItemStack::new('c', 3))
+	);
+}
+
+#[test]
+fn auto_stack_slotsize() {
+	let mut inv = Inv::<char>::new(1);
+	assert_eq!(
+		inv.auto_stack(ItemStack::new('c', 12)).unwrap_err(),
+		ItemStack::new('c', 9)
+	);
+}
+/*
+
+#[test]
 fn iterator() {
 	let mut inv = Inv::<char>::new(2);
-	inv.place_at(('x', 1), 0);
-	inv.place_at(('y', 3), 2);
+	inv.place_at(('x', 1), 0).ok();
+	inv.place_at(('y', 3), 2).ok();
 	
 	for (num, slot) in inv.into_iter().enumerate() {
 		if slot.is_some() {
@@ -27,182 +88,4 @@ fn iterator() {
 			assert!(slot.is_none())
 		}
 	}
-}
-
-#[test]
-fn slotsize() {
-	let mut inv = Inv::<char>::new(2);
-	inv.place_at(('x', 1), 0);
-	inv.place_at(('y', 3), 1);
-	assert_eq!(inv.place_at(('z', 3), 3), Some(('z', 3)));
-}
-
-#[test]
-fn slot_full() {
-	let mut inv = Inv::<char>::new(2);
-	inv.place_at(('x', 1), 0);
-	assert_eq!(inv.place_at(('y', 3), 0), Some(('y', 1)));
-}
-
-#[test]
-fn stacking() {
-	let mut inv = Inv::<char>::new(2);
-	assert_eq!(inv.stack(('x', 1)), None);
-	assert_eq!(inv.get_slot(0).unwrap().1, 1);
-	assert_eq!(inv.get_slot(1), None);
-	assert_eq!(inv.stack(('x', 1)), None);
-	assert_eq!(inv.get_slot(0).unwrap().1, 2);
-	assert_eq!(inv.get_slot(1), None);
-}
-
-#[test]
-fn stacksize_single() {
-	let mut inv = Inv::<char>::new(2);
-	assert_eq!(inv.place_at(('x', 10), 0), Some(('x', 7)));
-}
-
-#[test]
-fn find_free() {
-	let mut inv = Inv::<char>::new(3);
-	assert_eq!(inv.place_at(('x', 2), 0), None);
-	assert_eq!(inv.place_at(('y', 2), 2), None);
-	assert_eq!(inv.place_at_first_free(('z', 3)), None);
-	assert_eq!(inv.get_slot(1), Some(&('z', 3)));
-}
-
-#[test]
-fn stacksize_multiple() {
-	let mut inv = Inv::<char>::new(2);
-	assert_eq!(inv.place_at(('x', 1), 0), None);
-	assert_eq!(inv.place_at(('x', 1), 0), None);
-	assert_eq!(inv.place_at(('x', 1), 0), None);
-	assert_eq!(inv.place_at(('x', 1), 0), Some(('x', 1)));
-	assert_eq!(inv.get_slot(0).unwrap().1, 3);
-}
-
-#[test]
-fn stacksize_stack_overflow() {
-	let mut inv = Inv::<char>::new(2);
-	assert_eq!(inv.stack(('x', 3)), None);
-	assert_eq!(inv.stack(('x', 1)), None);
-	assert_eq!(inv.get_slot(1), Some(&('x', 1)));
-}
-
-#[test]
-fn find_item() {
-	let mut inv = Inv::<char>::new(3);
-	assert_eq!(inv.place_at(('x', 1), 1), None);
-	assert_eq!(inv.stack(('x', 1)), None);
-	assert_eq!(inv.get_slot(1).unwrap().1, 2);
-}
-
-#[test]
-fn selected_slot() {
-	let mut inv = Inv::<char>::new(300);
-	assert_eq!(inv.set_selected_slot(300), false);
-	assert_eq!(inv.set_selected_slot(2), true);
-	assert_eq!(inv.get_selected_slot(), None);
-	assert_eq!(inv.place_at(('z', 2), 2), None);
-	assert_eq!(inv.get_selected_slot(), Some(&('z', 2)));
-}
-
-#[test]
-fn fill_with_stack() {
-	let mut inv = Inv::<char>::new(4);
-	assert_eq!(inv.stack(('x', 12)), None);
-	for i in 0..4 {
-		assert_eq!(inv.get_slot(i), Some(&('x', 3)));
-	}
-	// These are 12 too many
-	assert_eq!(inv.stack(('u', 12)), Some(('u', 12)));
-	// These are 12 too many too
-	assert_eq!(inv.stack(('x', 12)), Some(('x', 12)));
-}
-
-#[test]
-fn fill_with_stack_place_first() {
-	let mut inv = Inv::<char>::new(4);
-	assert_eq!(inv.place_at(('x', 1), 1), None);
-	assert_eq!(inv.stack(('x', 11)), None);
-	for i in 0..4 {
-		assert_eq!(inv.get_slot(i), Some(&('x', 3)));
-	}
-}
-
-#[test]
-fn fill_with_place_at_first_free() {
-	let mut inv = Inv::<char>::new(6);
-	assert_eq!(inv.place_at_first_free(('z', 6)), None);
-	assert_eq!(inv.place_at_first_free(('x', 6)), None);
-	assert_eq!(inv.place_at_first_free(('z', 6)), None);
-	for i in 0..2 {
-		assert_eq!(inv.get_slot(i), Some(&('z', 3)));
-	}
-	for i in 2..4 {
-		assert_eq!(inv.get_slot(i), Some(&('x', 3)));
-	}
-	for i in 4..6 {
-		assert_eq!(inv.get_slot(i), Some(&('z', 3)));
-	}
-}
-
-#[test]
-fn main() {
-	// Create the Inventory with a slotsize of 4
-	let mut inv = Inv::<char>::new(4);
-	// inv-sys works with tuples of the item and an amount.
-	// place_at is used to try to place an item at an exact slot
-	assert_eq!(
-		//Try to place 1 'x' in slot 0
-		inv.place_at(('x', 1), 0), 
-		None
-	);
-	// It will return None, because the Item with its amount could successfully be placed in the inventory
-
-	// See what happens if you try to stack more items, than possible:
-	assert_eq!(
-		inv.place_at(('x', 3), 0), 
-		Some(('x',1))
-	);
-	// Yes, you have seen correctly! You got one Item back, 
-	// because the other ones were used to fill up the slot! 
-
-	// You can get an item from a slot like so:
-	// (and we can make sure there are really three Items in slot 0)
-	assert_eq!(
-		inv.get_slot(0), 
-		Some(&('x', 3))
-	);
-
-	// You can also stack items quickly. Stack will look for an incomplete stack of items and fill it first.
-	// It will then try to fill the next empty slots, beginning from the start
-	assert_eq!(
-		inv.stack(('x', 3)), 
-		None
-	);
-
-	// Fresh Start
-	inv = Inv::<char>::new(4);
-	// place_at_first_free will only look for free slots, and fill them.
-	inv.place_at(('x', 1), 0);
-	inv.place_at_first_free(('x', 2));
-	assert_eq!(
-		inv.get_slot(1), 
-		Some(&('x', 2))
-	);
-
-	// You can also set a selected slot
-	inv.set_selected_slot(1);
-	// And decrease the Item Counter
-	inv.decrease_selected_slot();
-	assert_eq!(
-		inv.get_slot(1), 
-		Some(&('x', 1))
-	);
-	// The Item will then become None at amount 0
-	inv.decrease_selected_slot();
-	assert_eq!(
-		inv.get_slot(1), 
-		None
-	);
-}
+}*/
